f766216485c22797ff8b11865eea28a1aba78a4a
GUZMAN_SENA <tobiclan98@gmail.com>
2026-02-01 09:25:24 -0500

    boto de pausa

---
+++ GamJamGlobal/Assets/Scripts/Sammy/UI/botonPausa.cs
@@ -0,0 +1,36 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class botonPausa : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+
+    public void PauseGame()
+
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        pauseButton.SetActive(false);
+
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale += 1;
+        pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+
+    public void RestarGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Salir del juego");
+        Application.Quit();
+    }
+}
