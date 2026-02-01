d57e8fc20d65244d898c13fc3ca1e957462faa40
Sebastian Franco <regsebas@gmail.com>
2026-02-01 09:21:13 -0500

    Cambios de dificultad

--- GamJamGlobal/Assets/B_Extension/Advance/SceneLoader/Scripts/ButtonQuitApplication.cs
+++ GamJamGlobal/Assets/B_Extension/Advance/SceneLoader/Scripts/ButtonQuitApplication.cs
@@ -1,7 +1,7 @@
 ﻿using B_Extensions;
 using B_Extensions.SceneLoader;
 
-public class ButtonQuickApplication: BaseButtonAttendant
+public class ButtonQuitApplication: BaseButtonAttendant
 {
     private void Start() => buttonComponent.onClick.AddListener(()=>SceneLoader.Instance.Quit());
 }
